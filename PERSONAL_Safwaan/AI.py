fc6f99aa6b6817f1d2cbe296fe403c1cd5771f6f
BiggusGiticus <150800977+BiggusGiticus@users.noreply.github.com>
2023-11-17 11:18:57 +0000

    Update AI.py
    first update in the computer room

--- PERSONAL_Safwaan/AI.py
+++ PERSONAL_Safwaan/AI.py
@@ -1,1 +1,37 @@
-import random as rand
+# UH DONT TOUCH THIS YET IM WORKING ON IT
+
+class AI:
+    def __init__(self, playerclick):
+        self.playerclick = playerclick
+
+    def setBoard():
+        global board = [
+            ["N","N","N"],
+            ["N","N","N"],
+            ["N","N","N"]
+        ]
+
+        # global num_rows = len(board)
+        # global num_cols = len(board[0])
+
+    def selectRandomPos():
+        from random import randint
+
+        coords_list = ["00","01","02","10","11","12","20","21","22"]
+        # random_row = randint(0, num_rows - 1)
+        # random_col = randint(0, num_cols - 1)
+
+        # return board[random_row][random_col]
+        
+    def startAI():
+        selectRandomPos()
+
+    def choosePlayer():
+        from random import randint
+        player_choose = randint(0,1)
+
+        if player_choose == 0:
+            #AI starts - link the startAI function here
+        if player_choose == 1:
+            #Player starts
+          
