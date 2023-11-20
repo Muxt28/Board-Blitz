8695d8c9a219c6d76d0fbf3f49dbdf454a5c7edb
BiggusGiticus <150800977+BiggusGiticus@users.noreply.github.com>
2023-11-20 10:46:56 +0000

    classwork

--- PERSONAL_Safwaan/AI.py
+++ PERSONAL_Safwaan/AI.py
@@ -6,6 +6,7 @@ class AI:
         self.board = [['-' for columns in range(3)] for rows in range(3)]
         self.ValidCoordinates = ['00', '01', '02', '10', '11', '12', '20', '21', '22']
         self.startPos = ['00', '02', '11', '20', '22']
+        
 
         self.player_choose = randint(0,1)
 
@@ -22,15 +23,29 @@ class AI:
         self.board[2][1] = "O"
     def move12(self):
         self.board[1][2] = "O"
+    
+
+    #These occur when
+    def move00(self):
+        self.board[self.startPos[0[0]]][self.startPos[0[1]]] = "X"
+    def move01(self):
+        move = self.startPos[1]
+        self.board[move[0]][move[1]] = "X"
 
     def choosePlayer(self):
         if self.player_choose == 0:
             self.startAI()
         else:
             #player starts
+            pass
 
     def playerStart(self):
-        if #p
+        choose = randint(0,1)
+        self.move00()
+        #after opponent moves
+        self.move
+
+
     def aiStart(self):
 
 
