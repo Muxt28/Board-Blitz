c01eb021de2755209d017ac3e48cc72e1acc955b
Muqsit <149266459+Muxt28@users.noreply.github.com>
2023-11-21 12:38:27 +0000

    Update GameManager.py

--- PERSONAL_Muqsit/src/Frontend/GameManager.py
+++ PERSONAL_Muqsit/src/Frontend/GameManager.py
@@ -5,12 +5,11 @@ import ursina.camera as camera
 import ursina.window as window
 
 import sys
+from multiprocessing import ThreadPool
 
 sys.path.append("..")
-from Backend import Library_DRAFT
+from Backend import Library
 from Frontend import Models
-
-from multiprocessing.pool import ThreadPool
 
 
 STATES = {
@@ -37,7 +36,6 @@ class Menu():
         ursina.camera.position = self.ROOTCAMERAPOS
         ursina.camera.rotation = self.ROOTCAMERAROT
         self.Water.position = self.Water.position + (0,-1.1,0)
-
         
     def showMainUI(self):
         self.PlayButton = ursina.Button(scale = (.2,.1), text = "Play", position = (-.4,0))
@@ -95,7 +93,7 @@ class Menu():
 class ThreeXThreeBoardScene():
     def __init__(self) -> None:
         STATES["In3x3Single"] = True
-        self.Board = ursina.Entity(model=Models.GetModelPath("3x3"), shader=shaders.basic_lighting_shader, color=ursina.color.rgb(255, 226, 200), scale=10)
+        self.Board = ursina.Entity(model=Models.GetModelPath("3x3"), collider = "box", shader=shaders.basic_lighting_shader, color=ursina.color.rgb(255, 226, 200), scale=10, onclick = self.onBoardClick)
         self.Back = ursina.Button(scale = (.07, .07/(16/14)), text = "Exit", position = ursina.window.top_left, origin = (-1,1))
         self.Back.on_click = self.destroy
         self.Back.text_entity.scale = 14
@@ -106,11 +104,13 @@ class ThreeXThreeBoardScene():
             origin = (0,1),
             scale = 1.6
             )
-        
+
         self.BoxesFilled = 0
         
         self.startGame()
         #self.Text = ursina.Text("The game is starting soon...")
+    def onBoardClick(self):
+        print("clicked board..")
 
     def setStatusText(self, text):
         self.StatusText.text = text
@@ -120,36 +120,86 @@ class ThreeXThreeBoardScene():
             print("The game instance has already started!")
             return 1
         self.hasGameStarted = True
-        # locked session
-        # self.inputQueue = Queue(maxsize=0)
-        # newGameInstance = Thread(target=Library.LocalPlayer, args=(self.inputQueue,))
-        # #newGameInstance.__UserInput = __UserInputFrontend
-        # win, board = newGameInstance.start()
-        while self.BoxesFilled < 9:
-            try:
-                thread = ThreadPool(processes=1)
-                threadReturn = thread.apply_async(Library_DRAFT.LocalPlayer.GamePlay, (Library_DRAFT.LocalPlayer(),))
-                x, y = threadReturn.get()
-                
-                print(x, y)
-                print('Working')
-                self.BoxesFilled += 1
-            except Exception as e :
-                print(e)
-                print('MOVE NOT VALID')
-                break
         
-        # print("started game thread from GameManager")
+        print("started game thread from GameManager")
+        self.CurrentTurn = "X"
+    
+    def getPosFromCoords(self, gameCoord):
+        # gives in mouse coords, returns board coords
+        print(gameCoord)
+        XROW = 0;
+        YROW = 0
+        if gameCoord.X <= -55:
+            XROW = 0
+        elif gameCoord.X > -55 and gameCoord.X <= 50:
+            XROW = 1
+        elif gameCoord.X > 50:
+            XROW = 2
+        if gameCoord.Z <= -55:
+            YROW = 2
+        elif gameCoord.Z > -55 and gameCoord.Z <= 50:
+            YROW = 1
+        elif gameCoord.Z > 50:
+            YROW = 0
+        gameCoords = str(YROW) + str(XROW)
+        coordDict = {
+            "00" : (-107,7,107),
+            "01" : (0,7,107),
+            "02" : (107,7,107),
+            "10" : (-107,7,0),
+            "11" : (0,7,0),
+            "12" : (107,7,0),
+            "20" : (-107,7,-107),
+            "21" : (0,7,-107),
+            "22" : (107,7,-107),
+        }
+        return coordDict[gameCoords]
+
+    def placeX(self, coords):
+        newX = ursina.Entity(model=Models.GetModelPath("X"), shader=shaders.basic_lighting_shader, scale=10)
+        newX.position = self.getPosFromCoords(coords)    
+        # position newX with coords
+        # handle 3d stuff here... and THEN 
+
+        # do ur backend ting here
+    
+    def placeO(self, coords):
+        # handle 3d stuff here.. and then
+        newO = ursina.Entity(model=Models.GetModelPath("O"), shader=shaders.basic_lighting_shader, scale=10)
+        newO.position = self.getPosFromCoords(coords)    
+        pass
+        # backend
+
+    def handleMouseClick(self, pos, BoxesFilled):
+        print("left mouse  button clicked!")
         
+        thread = ThreadPool(processes=1)
+        threadReturn = thread.apply_async(Library.LocalPlayer.GamePlay, (Library.LocalPlayer(self.BoxesFilled, ),))
+        win, currentPlayer = threadReturn.get()
+        print(win, currentPlayer)
+
+        # if valid != 'VALID':
+        #     return 'NOT VALID'
+
+        if pos != None:
+            if BoxesFilled % 2 == 0:
+                self.placeX(pos)
+            else:
+                self.placeO(pos)
+
+        if win  == '*[ Player 1 has Won ]*' or win == '*[ Player 2 has Won ]*':
+            return win
+
+
     def destroy(self):
         global MENU_GLOBAL
         STATES["In3x3Single"] = False
         ursina.destroy(self.Board)
         ursina.destroy(self.Back)
         ursina.destroy(self.StatusText)
-
         MENU_GLOBAL = Menu(False)
 
-    def onUpdate(self):
-        
+    def onUpdate(self, mouseCoords):
+        if mouseCoords!=None:
+            print(mouseCoords)
         pass
